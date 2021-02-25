d057d70c81ddd72efe3862f93fff4b3c5f203eb7
Aaron Brennan <aaron.brennan@bryanuniversity.edu>
2021-02-25 08:48:05 -0800

    Name Entry

--- week4/assignment-1/assignment-1/src/App.js
+++ week4/assignment-1/assignment-1/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from "react"
+import Entry from "./components/Entry"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+        name: "",
+        list: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleChange(event) {
+    const {name, value} = event.target
+    this.setState({
+      name: value
+    })
+    event.preventDefault();
+  }
+  
+  handleClick(event) {
+    this.setState(prevState => ({
+      list: [...prevState.list, this.state.name]
+    })
+  )
+    event.preventDefault();
 }
+
+  render () {
+    const people = this.state.list.map(x => <Entry name={x} />)
+    return (
+      <div>
+        <form>
+            <input 
+                type="text" 
+                name="name" 
+                placeholder="Name" 
+                onChange={this.handleChange} 
+            />
+            <br />
+            <br />
+            <button onClick={this.handleClick}>Submit</button>
+            <h1>{this.state.name}</h1>
+            {people}
+        </form>
+      </div>
+    )
+  }
+}
+
 
 export default App;
---
+++ week4/assignment-1/assignment-1/src/components/Entry.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Name (props) {
+    return (
+        <div>
+            <ul>
+                <li>{props.name}</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Name
